e9390c96c28be95385535bc66e27043466652ad9
kathir56 <akathir2004@gmail.com>
2024-09-14 21:29:54 +0530

    All requirements done!

---
+++ Vought Showcase/Utilities/ActionButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  ActionButtonView.swift
+//  Vought Showcase
+//
+//  Created by Kathiravan Murali on 14/09/24.
+//
+
+import UIKit
+
+class ActionButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
